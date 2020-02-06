f5f8abbf78d2ed4a3c2e73ecc3fff506143285d8
Brown <lbrown11@cincinnatistate.edu>
2020-02-05 21:56:24 -0500

    finished ass5

--- assignment5While.py
+++ assignment5While.py
@@ -1,15 +1,15 @@
 #--------------------------------------------
 #Lance Brown
-#Assignment 5 For
+#Assignment 5 While
 #---------------------------------------------
-
-#-----------------------------------------------
-#For loop
-#------------------------------------------------
+#---------------------------------------------------------------
+#While Loop
+#-------------------------------------------------------
 dblRate = float(input("Enter your current hourly pay: "))
 dblRaise = float(input("Enter your expected annual raise percentage: "))
 dblAnnual = dblRate * 40 * 52
-for i in range(10):
+i = 0
+while i < 10:
 	print("Year %d - $%.2f" % ((i+1), dblAnnual))
 	dblAnnual *= 1 + (dblRaise * 0.01)
-	
+	i+=1
