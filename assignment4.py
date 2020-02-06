7131bafae8ae31b993e804a480fa00827df71171
Brown <lbrown11@cincinnatistate.edu>
2020-02-05 21:41:42 -0500

    finished assignment 4

--- assignment4.py
+++ assignment4.py
@@ -7,15 +7,15 @@
 #-----------------------------------------------------
 # Problem 1 - Print the largest of four numbers
 #-------------------------------------------------------
-# print("Please enter 4 whole numbers: ")
-# arrNumbers = list()
-# for i in range(4):
-	# intNumber = input("Number " + str(i + 1) + ": ")
-	# arrNumbers.append(int(intNumber))
-# arrNumbers.sort(reverse = True)
-# print(str(arrNumbers[0]) + " is the largest number provided.")
+print("Please enter 4 whole numbers: ")
+arrNumbers = list()
+for i in range(4):
+	intNumber = input("Number %d: " % (i + 1))
+	arrNumbers.append(int(intNumber))
+arrNumbers.sort(reverse = True)
+print("%d is the largest number provided." % arrNumbers[0])
 #------------------------------------------------------------------
-#Problem 2 - Determine if a year is a leap year
+#Problem 2 - Print whether a year is a leap year
 #----------------------------------------------------------------------
 
 intYear = int(input("Enter a year: "))
@@ -23,11 +23,28 @@ if intYear <= 1500 or intYear >= 2200:
 	print("Invalid Year")
 elif intYear % 100 == 0:
 	if intYear % 400 == 0:
-		print("Yes, " + str(intYear) + " is a leap year.")
+		print("Yes, %d is a leap year." % intYear)
 	else:
-		print("No, " + str(intYear) + " is not a leap year.")
+		print("No, %d is not a leap year." % intYear)
 elif intYear % 4 == 0:
-	print("Yes, " + str(intYear) + " is a leap year.")
+	print("Yes, %d is a leap year." % intYear)
 else:
-	print("No, " + str(intYear) + " is not a leap year.")
+	print("No, %d is not a leap year." % intYear)
 	
+#----------------------------------------------------------------------
+#Problem 3 - Print Net pay
+#---------------------------------------------------------------------
+dblRate = float(input("Enter your hourly pay rate: "))
+dblHours = float(input("Enter hours worked: "))
+if dblRate > 0 and dblHours > 0:
+	dblGross = dblRate * dblHours
+	if dblGross <= 300:
+		dblTax = dblGross * 0.15
+	elif dblGross <=450:
+		dblTax = ((dblGross - 300) * 0.2) + 45
+	else:
+		dblTax = ((dblGross - 450) * .25) + 75
+	dblNet = dblGross - dblTax
+	print("Net pay: $%.2f" % dblNet)
+else:
+	print("Invalid input")
