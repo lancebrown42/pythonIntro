85b867fe1789a6fd105afcfb375f2e23549d5e9b
Brown <lbrown11@cincinnatistate.edu>
2020-02-12 22:07:53 -0500

    started assignment 6

---
+++ assignment6.py
@@ -0,0 +1,44 @@
+#########################################################
+#Lance Brown
+#Assignment 6
+#02/12/20
+#########################################################
+
+# -----------------------------------------------------------------
+# Function Name:        Display Instructions
+# Function Purpose:     Displays instructions to the user
+# -----------------------------------------------------------------
+def DisplayInstructions():
+	print("This program will demonstrate how to make and use procedures in Python... \n
+	In addition it will demonstrate how to pass values and variables into \n
+	a procedure as parameters.  It will demonstrate Python deals with ByRef and ByVal."
+	return none
+
+
+
+# -----------------------------------------------------------------
+# Function Name:        Display Message
+# Function Purpose:     Displays a set message a given number of times
+# -----------------------------------------------------------------
+def DisplayMessage(intPrintCount):
+	for i in range(intPrintCount):
+		print("I Love Python\n")
+	return none
+		
+		
+
+# -----------------------------------------------------------------
+# Function Name:        Get Larger Value
+# Function Purpose:     Finds the larger of two values
+# -----------------------------------------------------------------
+
+
+
+
+# -----------------------------------------------------------------
+# Function Name:        Main
+# Function Purpose:     Controls flow of program
+# -----------------------------------------------------------------
+intPrintCount = 2
+DisplayInstructions()
+DisplayMessage(intPrintCount)
