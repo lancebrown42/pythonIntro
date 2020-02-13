40a1d6182fea8560d4de56959f9fca35a1ca9354
Brown <lbrown11@cincinnatistate.edu>
2020-02-12 23:08:26 -0500

    finished 6

--- assignment6.py
+++ assignment6.py
@@ -9,10 +9,23 @@
 # Function Purpose:     Displays instructions to the user
 # -----------------------------------------------------------------
 def DisplayInstructions():
-	print("This program will demonstrate how to make and use procedures in Python... \n
-	In addition it will demonstrate how to pass values and variables into \n
-	a procedure as parameters.  It will demonstrate Python deals with ByRef and ByVal."
-	return none
+	print("This program will demonstrate how to make and use procedures in Python... \n In addition it will demonstrate how to pass values and variables into \n a procedure as parameters.  It will demonstrate Python deals with ByRef and ByVal.")
+	return
+
+
+
+# -----------------------------------------------------------------
+# Function Name:        Validate Integer
+# Function Purpose:     Validates that inputs are positive integers
+# -----------------------------------------------------------------
+def ValidateInteger(*intValuev):
+	blnValid = False
+	for intValue in intValuev:	
+		if intValue > 0 and isinstance(intValue, int):
+			blnValid = True
+		else:
+			return False
+	return blnValid
 
 
 
@@ -23,7 +36,7 @@ def DisplayInstructions():
 def DisplayMessage(intPrintCount):
 	for i in range(intPrintCount):
 		print("I Love Python\n")
-	return none
+	return
 		
 		
 
@@ -31,7 +44,41 @@ def DisplayMessage(intPrintCount):
 # Function Name:        Get Larger Value
 # Function Purpose:     Finds the larger of two values
 # -----------------------------------------------------------------
+def GetLargerValue(intValue1, intValue2):
+	if intValue1 > intValue2:
+		return intValue1
+	elif intValue2 > intValue1:
+		return intValue2
+	elif intValue1 == intValue2:
+		return intValue1
+	else:
+		print("Something has gone horribly wrong")
+		return
 
+
+
+# -----------------------------------------------------------------
+# Function Name:        Get Largest Value
+# Function Purpose:     Finds the largest of seven values
+# -----------------------------------------------------------------
+def GetLargestValue(intValue1, intValue2, intValue3, intValue4, intValue5, intValue6, intValue7):
+	intMax = GetLargerValue(intValue1, intValue2)
+	intMax = GetLargerValue(intMax, intValue3)
+	intMax = GetLargerValue(intMax, intValue4)
+	intMax = GetLargerValue(intMax, intValue5)
+	intMax = GetLargerValue(intMax, intValue6)
+	intMax = GetLargerValue(intMax, intValue7)
+	return intMax
+
+
+
+# -----------------------------------------------------------------
+# Function Name:        Calculate Sphere Volume
+# Function Purpose:     Calculates volume of a sphere from given diameter
+# -----------------------------------------------------------------
+def CalculateSphereVolume(intDiameter):
+	dblVolume = (4 / 3) * 3.1415 * (intDiameter / 2) ** 3
+	return dblVolume
 
 
 
@@ -39,6 +86,25 @@ def DisplayMessage(intPrintCount):
 # Function Name:        Main
 # Function Purpose:     Controls flow of program
 # -----------------------------------------------------------------
-intPrintCount = 2
 DisplayInstructions()
-DisplayMessage(intPrintCount)
+intPrintCount = int(input("Enter print count: "))
+if ValidateInteger(intPrintCount):
+	DisplayMessage(intPrintCount)
+else:
+	print("Invalid input")
+intValue1, intValue2 = input("Enter two positive integers: ").split()
+if ValidateInteger(int(intValue1), int(intValue2)):
+	print(GetLargerValue(int(intValue1), int(intValue2)))
+
+else:
+	print("Invalid input")
+intValue1, intValue2, intValue3, intValue4, intValue5, intValue6, intValue7 = input("Enter seven positive integers: ").split()
+if ValidateInteger(int(intValue1), int(intValue2), int(intValue3), int(intValue4), int(intValue5), int(intValue6), int(intValue7)):
+	print(GetLargestValue(int(intValue1), int(intValue2), int(intValue3), int(intValue4), int(intValue5), int(intValue6), int(intValue7)))
+else:
+	print("Invalid input")
+intDiameter = int(input("Enter the diameter of a sphere: "))
+if ValidateInteger(intDiameter):
+	print(CalculateSphereVolume(intDiameter))
+else:
+	print("Invalid input")
